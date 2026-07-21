5d4515852da47e1266a14a54f012c834bfa2d86a
Aiyshah <maiyshah6@gmail.com>
2026-07-21 22:26:30 +0500

    Day 7: Added JavaScript events and scroll-to-top button

---
+++ script.js
@@ -0,0 +1,26 @@
+const hireButton = document.querySelector("#home button");
+
+hireButton.addEventListener("click", function () {
+    alert("Thank you for your interest!");
+});
+
+const topButton = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 200) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+
+});
+
+topButton.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
